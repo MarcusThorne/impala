cb6af48d6fb908d08cf46b35fc614f9cb39ba18a
Marcus Thorne <marcus_thorne475@hotmail.com>
2021-01-14 23:02:06 +0000

    chnaged the layout of the blog posts, added some links to the homepage and chnaged the footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,22 @@ class PagesController < ApplicationController
     @blogs = [
       { title: 'Title of a blog that someone wrote', image: 'bar2', source: 'Sourced from blah / 23 days ago' }, 
       { title: 'A completely different title of a blog that someone wrote', image: 'people', source: 'Sourced from blah / 23 days ago' }, 
-      { title: 'An even different title of a blog that someone wrote', image: 'food', source: 'Sourced from blah / 43 days ago' }]
+      { title: 'An even different title of a blog that someone wrote', image: 'food', source: 'Sourced from blah / 43 days ago' },
+      { title: 'Awesome Blog post', image: 'food', source: 'Sourced from wikapedia / 76 days ago' },
+      { title: 'Title of a blog that someone wrote', image: 'bar2', source: 'Sourced from blah / 23 days ago' }, 
+      { title: 'A completely different title of a blog that someone wrote', image: 'people', source: 'Sourced from blah / 23 days ago' }, 
+      { title: 'An even different title of a blog that someone wrote', image: 'food', source: 'Sourced from blah / 43 days ago' },
+      { title: 'Awesome Blog post', image: 'food', source: 'Sourced from wikapedia / 76 days ago' },
+      { title: 'Title of a blog that someone wrote', image: 'bar2', source: 'Sourced from blah / 23 days ago' }, 
+      { title: 'A completely different title of a blog that someone wrote', image: 'people', source: 'Sourced from blah / 23 days ago' }, 
+      { title: 'An even different title of a blog that someone wrote', image: 'food', source: 'Sourced from blah / 43 days ago' },
+      { title: 'Awesome Blog post', image: 'food', source: 'Sourced from wikapedia / 76 days ago' }
+    ]
+    @navLinks = [{ title: 'stay', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab nam architecto odit beatae rerum nesciunt maiores eligendi, perferendis velit eum quo cumque officia! Fuga saepe, enim vel excepturi molestias fugit.' }, 
+      { title: 'eat', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab nam architecto odit beatae rerum nesciunt maiores eligendi, perferendis velit eum quo cumque officia! Fuga saepe, enim vel excepturi molestias fugit.' },
+      { title: 'drink', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab nam architecto odit beatae rerum nesciunt maiores eligendi, perferendis velit eum quo cumque officia! Fuga saepe, enim vel excepturi molestias fugit.' },
+      { title: 'explore', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab nam architecto odit beatae rerum nesciunt maiores eligendi, perferendis velit eum quo cumque officia! Fuga saepe, enim vel excepturi molestias fugit.' }
+    ]
+    @options = [{ title: 'Check-In', date: '10/10/12' }, { title: 'Check-Out', date: '10/10/12' }, { title: 'Adults', date: '1 Adult' }, { title: 'Children', date: '0 Children' }]
   end
 end
