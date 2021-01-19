9ed3014eee83e46d64b814be0f7ac5532be6ab95
Marcus Thorne <marcus_thorne475@hotmail.com>
2021-01-19 20:41:36 +0000

    homepage edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   def set_categories
     @navLinks = [{ title: 'stay', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab nam architecto odit beatae rerum nesciunt maiores eligendi, perferendis velit eum quo cumque officia! Fuga saepe, enim vel excepturi molestias fugit.', image: '/assets/stay.jpg' }, 
-      { title: 'eat', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab nam architecto odit beatae rerum nesciunt maiores eligendi, perferendis velit eum quo cumque officia! Fuga saepe, enim vel excepturi molestias fugit.', image: '/assets/eat.jpg' },
-      { title: 'drink', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab nam architecto odit beatae rerum nesciunt maiores eligendi, perferendis velit eum quo cumque officia! Fuga saepe, enim vel excepturi molestias fugit.', image: '/assets/drink.jpg' },
-      { title: 'explore', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab nam architecto odit beatae rerum nesciunt maiores eligendi, perferendis velit eum quo cumque officia! Fuga saepe, enim vel excepturi molestias fugit.', image: '/assets/explore.jpg' }
+      { title: 'eat & drink', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab nam architecto odit beatae rerum nesciunt maiores eligendi, perferendis velit eum quo cumque officia! Fuga saepe, enim vel excepturi molestias fugit.', image: '/assets/eat.jpg' },
+      { title: 'about us', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab nam architecto odit beatae rerum nesciunt maiores eligendi, perferendis velit eum quo cumque officia! Fuga saepe, enim vel excepturi molestias fugit.', image: '/assets/drink.jpg' },
+      { title: 'contact us', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab nam architecto odit beatae rerum nesciunt maiores eligendi, perferendis velit eum quo cumque officia! Fuga saepe, enim vel excepturi molestias fugit.', image: '/assets/explore.jpg' }
     ]
   end
 end
