ebcbb9dfe4742fe7990746ff09aceed7077debd8
Marcus Thorne <marcus_thorne475@hotmail.com>
2021-01-21 21:54:39 +0000

    styled the avilabilty page and created a way of retrieving only the rooms that have availability on the dates selected

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,12 +1,40 @@
 class AvailabilitiesController < ApplicationController
   def index
     @booking = Booking.new
-
-    if params[:guests].present?  
-      @rooms = Room.select { |r| r.beds >= params[:guests].to_i }
+    
+    if params[:guests].present? && params[:check_in].present? && params[:check_out].present?  
+      @rooms = Room.select do |r|
+        guest_count = 0 
+        r.bookings.each do |bk|
+          guest_count += bk.guests if bk.check_in >= params[:check_in].to_date && bk.check_out <= params[:check_out].to_date
+        end
+        r if (r.beds - guest_count) >= params[:guests].to_i     
+      end
+    elsif params[:check_in].present? && params[:check_out].present?
+      @rooms = Room.select do |r|
+        guest_count = 0
+        r.bookngs.each do |bk|
+          guest_count += bk.guests
+        end
+        r if (r.beds - guest_count) > 0
+      end
+    elsif params[:guests].present?
+      @rooms = Room.select do |r|
+        guest_count = 0
+        r.bookings.each do |bk|
+          guest_count += bk.guests if bk.check_in == date.today && bk.check_out == date.tomorrow
+        end
+        r if (r.beds - guest_count) >= params[:guests].to_i
+      end
     else
       @rooms = Room.all
     end
+    
+    @privateRoom = @rooms.select { |r| r.name.downcase == 'private room' }
+    @twoBedDorm = @rooms.select { |r| r.name.downcase == '2 bed dorm' }
+    @fourBedDorm = @rooms.select { |r| r.name.downcase == '4 bed dorm' }
+    @eightBedDorm = @rooms.select { |r| r.name.downcase == '8 bed dorm' }
+    @allRooms = [@privateRoom, @twoBedDorm, @fourBedDorm, @eightBedDorm]
   end
 
   def search
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-number = (1..50).to_a
 
-number.each do |n|
-  room = Room.create!(room_number: n, name: (Faker::Number.within(range: 4..12)).to_s + " Bed Dorm", beds: Faker::Number.within(range: 4..12), floor: Faker::Number.within(range: 0..8), private_bathroom: true || false)
-  puts "Room number #{room.room_number} created"
-end
+Room.create!(name: 'Private Room', beds: 1, floor: 5, private_bathroom: true, room_number: 1 )
+puts('First Private 1 bed room created')
+Room.create!(name: 'Private Room', beds: 1, floor: 5, private_bathroom: true, room_number: 2 )
+puts('Second Private 1 bed room created')
+Room.create!(name: 'Private Room', beds: 1, floor: 5, private_bathroom: true, room_number: 3 )
+puts('Third Private 1 bed room created')
+Room.create!(name: 'Private Room', beds: 1, floor: 5, private_bathroom: true, room_number: 4 )
+puts('Forth Private 1 bed room created')
+Room.create!(name: '2 Bed Dorm', beds: 2, floor: 1, private_bathroom: false, room_number: 5 )
+puts('First 2 bed dorm created')
+Room.create!(name: '2 Bed Dorm', beds: 2, floor: 1, private_bathroom: false, room_number: 6 )
+puts('Second 2 bed dorm created')
+Room.create!(name: '4 Bed Dorm', beds: 4, floor: 2, private_bathroom: false, room_number: 7 )
+puts('First 4 bed dorm created')
+Room.create!(name: '4 Bed Dorm', beds: 4, floor: 2, private_bathroom: false, room_number: 8 )
+puts('Second 4 bed dorm created')
+Room.create!(name: '8 Bed Dorm', beds: 8, floor: 3, private_bathroom: false, room_number: 9 )
+puts('First 8 bed dorm created')
+Room.create!(name: '8 Bed Dorm', beds: 8, floor: 4, private_bathroom: false, room_number: 10 )
+puts('Second 8 bed dorm created')
