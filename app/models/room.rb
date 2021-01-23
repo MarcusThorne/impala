fe2d8e3a57bc68cf9f6f140e1e7f9da703e03615
Marcus Thorne <marcus_thorne475@hotmail.com>
2021-01-23 23:36:45 +0000

    file tweaks

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,15 +1,3 @@
 class Room < ApplicationRecord
   has_many :bookings, dependent: :destroy
-
-  def self.find_title(i)
-    if i = 0 
-      then "Private Room's" 
-    elsif i = 1 
-      then "2 Bed Dorm's" 
-    elsif i = 2 
-      then "4 Bed Dorm's" 
-    elsif i = 3 
-      then "8 Bed Dorm's" 
-    end 
-  end
 end
