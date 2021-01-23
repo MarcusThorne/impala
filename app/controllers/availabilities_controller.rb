b188e137a3940a12879ae97b467fada4d9baa723
marcusthorne <marcus_thorne475@hotmail.com>
2021-01-23 00:56:00 +0000

    programmed the search filter from the home page to link to the availbilty page and search for rooms

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,35 +1,44 @@
 class AvailabilitiesController < ApplicationController
   def index
     @booking = Booking.new
-    
-    if params[:guests].present? && params[:check_in].present? && params[:check_out].present?  
+
+    if params[:check_in].present? && params[:check_out].present? && params[:guests].present?
       @rooms = Room.select do |r|
-        guest_count = 0 
-        r.bookings.each do |bk|
-          guest_count += bk.guests if bk.check_in >= params[:check_in].to_date && bk.check_out <= params[:check_out].to_date
+        guest_count = 0
+        unless r.bookings.nil? do
+          r.bookings.each do |bk|
+            guest_count += bk.guests if bk.check_in >= params[:check_in].to_date && bk.check_out <= params[:check_out].to_date
+          end
         end
-        r if (r.beds - guest_count) >= params[:guests].to_i     
+        r if (r.beds - guest_count) >= params[:guests].to_i
+        end
       end
     elsif params[:check_in].present? && params[:check_out].present?
       @rooms = Room.select do |r|
         guest_count = 0
-        r.bookngs.each do |bk|
-          guest_count += bk.guests
+        unless r.bookings.nil? do
+          r.bookngs.each do |bk|
+            guest_count += bk.guests
+          end
         end
         r if (r.beds - guest_count) > 0
+        end
       end
     elsif params[:guests].present?
       @rooms = Room.select do |r|
         guest_count = 0
-        r.bookings.each do |bk|
-          guest_count += bk.guests if bk.check_in == date.today && bk.check_out == date.tomorrow
+        unless r.bookings.nil? do
+          r.bookings.each do |bk|
+            guest_count += bk.guests if bk.check_in == date.today && bk.check_out == date.tomorrow
+          end
         end
         r if (r.beds - guest_count) >= params[:guests].to_i
+        end
       end
     else
       @rooms = Room.all
     end
-    
+
     @privateRoom = @rooms.select { |r| r.name.downcase == 'private room' }
     @twoBedDorm = @rooms.select { |r| r.name.downcase == '2 bed dorm' }
     @fourBedDorm = @rooms.select { |r| r.name.downcase == '4 bed dorm' }
@@ -38,6 +47,6 @@ class AvailabilitiesController < ApplicationController
   end
 
   def search
-    
+
   end
 end
--- app/javascript/components/changeNavbar.js
+++ app/javascript/components/changeNavbar.js
@@ -1,12 +1,11 @@
 const changeNavbar = () => {
   window.addEventListener('scroll', function() {
-    let navbar = document.querySelector(".navbar-container")
-    let collapse = document.querySelector(".content-collapsed")
+    let navbar = document.querySelector(".navbar")
+    let collapse = document.querySelector(".collapse-container")
     let windowPosition = scrollY > 0
-    
-    navbar.classList.toggle('scrolling-active', windowPosition)
+
+    navbar.classList.toggle('scrolling-active-navbar', windowPosition)
     collapse.classList.toggle('scrolling-active-collapase', windowPosition)
-    console.log("Scrolling Active")
   })
 }
 
